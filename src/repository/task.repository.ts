9a1c74ecbba98f236cbc237bcaec58884e53261b
NIRANJAN B <niranjannb7777@gmail.com>
2023-08-19 14:25:47 +0530

    Get endpoint (#13)
    * Get Get By ID Endpoints
    
    * Refactor Logs

--- src/repository/task.repository.ts
+++ src/repository/task.repository.ts
@@ -3,6 +3,22 @@ import Task from "../entity/task.entity";
 
 class TaskRepository {
     constructor(private repository: Repository<Task>) {}
+
+    findTasks() : Promise<Task[]>{
+        return this.repository.find();
+    }
+
+    findTaskById(id) : Promise<Task>{
+        return this.repository.findOne({
+            where: {id},
+            relations:{
+                employees: true,
+                createdBy:true,
+                approvedBy:true,
+                taskcomments:true
+            }
+        })
+    }
 }
 
 export default TaskRepository;
