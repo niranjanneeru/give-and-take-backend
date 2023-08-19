40d665e9de7e2128976686193952450c3e8a80dd
NIRANJAN B <niranjannb7777@gmail.com>
2023-08-19 14:26:49 +0530

    Get endpoint (#14)
    * Get Get By ID Endpoints
    
    * Refactor Logs

--- src/service/task.service.ts
+++ src/service/task.service.ts
@@ -1,7 +1,21 @@
+import HttpException from "../exception/http.exception";
 import TaskRepository from "../repository/task.repository";
+import { StatusCodes } from "../utils/status.code.enum";
 
 class TaskService {
-    constructor(private service: TaskRepository) {}
+    constructor(private taskRepository: TaskRepository) {}
+
+    getTasks(){
+        return this.taskRepository.findTasks();
+    }
+
+    async getTaskById(id: string){
+        const task = await this.taskRepository.findTaskById(id);
+        if(!task){
+            throw new HttpException(StatusCodes.NOT_FOUND, `Task with id ${id} not found`);
+        }
+        return task;
+    }
 }
 
 export default TaskService;
