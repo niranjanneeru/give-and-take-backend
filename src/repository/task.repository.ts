eb5f8f1cda155337374fca2da4d3bc02a3916ca3
Vaishnav N T <68477941+Vaishnav-NT@users.noreply.github.com>
2023-08-22 00:25:10 +0530

    filter implemented (#34)

--- src/repository/task.repository.ts
+++ src/repository/task.repository.ts
@@ -1,9 +1,11 @@
 import { Repository } from "typeorm/repository/Repository";
 import Task from "../entity/task.entity";
 import Employee from "../entity/employee.entity";
+import { TaskStatus } from "../utils/taskStatus.enum";
+import { Raw } from "typeorm";
 
 class TaskRepository {
-    constructor(private repository: Repository<Task>) { }
+    constructor(private repository: Repository<Task>) {}
 
     createTask(task: Task): Promise<Task> {
         return this.repository.save(task);
@@ -12,18 +14,39 @@ class TaskRepository {
     findTasks(): Promise<Task[]> {
         return this.repository.find({
             relations: {
-                employees: true
-            }
+                employees: true,
+            },
         });
     }
 
+    findTasksByTaskCompletionStatus(filter: string): Promise<Task[]> {
+        return this.repository
+            .createQueryBuilder("task")
+            .where({ status: filter })
+            .getMany();
+    }
+
+    findExpiredTasks(): Promise<Task[]> {
+        return this.repository.findBy({
+            deadline: Raw((alias) => `${alias} < NOW()`),
+        });
+    }
+
+    findDirectBountyTasks(): Promise<Task[]> {
+        return this.repository
+            .createQueryBuilder("task")
+            .where({ isDirectBounty: true })
+            .getMany();
+    }
+
     findTaskById(id): Promise<Task> {
-        return this.repository.createQueryBuilder('task')
-            .leftJoinAndSelect('task.comments', 'comment')
-            .leftJoinAndSelect('comment.postedBy', 'employee')
-            .orderBy('comment.createdAt', 'DESC')
+        return this.repository
+            .createQueryBuilder("task")
+            .leftJoinAndSelect("task.comments", "comment")
+            .leftJoinAndSelect("comment.postedBy", "employee")
+            .orderBy("comment.createdAt", "DESC")
             .where({ id })
-            .getOne()
+            .getOne();
     }
 
     findTaskCreatorApprover(id: string): Promise<Task> {
@@ -31,18 +54,18 @@ class TaskRepository {
             where: { id },
             relations: {
                 approvedBy: true,
-                createdBy: true
-            }
-        })
+                createdBy: true,
+            },
+        });
     }
 
     findTaskAssignees(id: string): Promise<Task> {
         return this.repository.findOne({
             where: { id },
             relations: {
-                employees: true
-            }
-        })
+                employees: true,
+            },
+        });
     }
 
     updateTask(task: Task): Promise<Task> {
@@ -55,16 +78,13 @@ class TaskRepository {
     }
 
     removeAssigneesFromTask(task: Task, emp: Employee): Promise<Task> {
-        task.employees = task.employees.filter(e => e.id != emp.id)
+        task.employees = task.employees.filter((e) => e.id != emp.id);
         return this.repository.save(task);
     }
-
-
 
     removeTask(task: Task): Task | PromiseLike<Task> {
         return this.repository.softRemove(task);
     }
-
 }
 
 export default TaskRepository;
